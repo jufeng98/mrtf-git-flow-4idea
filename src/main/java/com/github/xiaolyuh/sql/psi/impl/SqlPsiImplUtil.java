f29967fae2d2dc61b8014e740d6273c33e107191
liangyudong <liangyudong@bluemoon.com.cn>
2024-07-01 18:23:09 +0800

    完善sql语言支持

--- src/main/java/com/github/xiaolyuh/sql/psi/impl/SqlPsiImplUtil.java
+++ src/main/java/com/github/xiaolyuh/sql/psi/impl/SqlPsiImplUtil.java
@@ -1,4 +1,14 @@
 package com.github.xiaolyuh.sql.psi.impl;
 
+import com.github.xiaolyuh.sql.psi.SqlStatement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
+import org.jetbrains.annotations.NotNull;
+
 public class SqlPsiImplUtil {
+
+    public static PsiReference @NotNull [] getReferences(SqlStatement param) {
+        return ReferenceProvidersRegistry.getReferencesFromProviders(param);
+    }
+
 }
