9c0663cb165f7f401aee938b371eca10a8ebe0e8
liangyudong <liangyudong@bluemoon.com.cn>
2024-07-21 17:46:24 +0800

    完善代码

--- src/main/java/com/github/xiaolyuh/sql/reference/ColumnTableAliasPsiReference.java
+++ src/main/java/com/github/xiaolyuh/sql/reference/ColumnTableAliasPsiReference.java
@@ -3,14 +3,11 @@ package com.github.xiaolyuh.sql.reference;
 import com.github.xiaolyuh.sql.psi.impl.FakePsiElement;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementResolveResult;
-import com.intellij.psi.PsiPolyVariantReference;
 import com.intellij.psi.PsiReferenceBase;
-import com.intellij.psi.ResolveResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ColumnTableAliasPsiReference extends PsiReferenceBase<PsiElement> implements PsiPolyVariantReference {
+public class ColumnTableAliasPsiReference extends PsiReferenceBase<PsiElement> {
     private final PsiElement targetPsiElement;
 
     public ColumnTableAliasPsiReference(@NotNull PsiElement element, @NotNull TextRange textRange, PsiElement targetPsiElement) {
@@ -23,16 +20,11 @@ public class ColumnTableAliasPsiReference extends PsiReferenceBase<PsiElement> i
         this.targetPsiElement = targetPsiElement;
     }
 
-    @Override
-    public ResolveResult @NotNull [] multiResolve(boolean incompleteCode) {
-        return new ResolveResult[]{new PsiElementResolveResult(targetPsiElement)};
-    }
 
     @Nullable
     @Override
     public PsiElement resolve() {
-        ResolveResult[] resolveResults = multiResolve(true);
-        return resolveResults.length == 1 ? resolveResults[0].getElement() : null;
+        return targetPsiElement;
     }
 
 }
