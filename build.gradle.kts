06a3e01ac1171f6fcd978b371da5284f9a8b2137
liangyudong <liangyudong@bluemoon.com.cn>
2025-05-22 18:07:10 +0800

    升级版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.xiaolyuh"
-version = "1.4.0"
+version = "2.0.0"
 
 repositories {
     maven { url = URI("https://maven.aliyun.com/nexus/content/groups/public/") }
